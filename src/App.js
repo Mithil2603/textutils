47338ea18400fcc52c827d0b3e595fd0d80bbb9f
Mithil Suthar <136597187+Mithil2603@users.noreply.github.com>
2024-08-19 14:19:13 +0530

    new functionalities added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from './Components/NavBar';
 import TextForm from './Components/TextForm';
+// import About from './Components/About';
 
 function App() {
   return (
     <>
       <NavBar title="TextUtils" aboutText="About TextUtils"/>
       <TextForm heading="Enter the text to analyse below" />
+      {/* <About></About> */}
     </>
   );
 }
